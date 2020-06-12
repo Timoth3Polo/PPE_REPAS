d105ef7d416998275e79b53c108263b51ffd01c6
Timoth3Polo <66690744+Timoth3Polo@users.noreply.github.com>
2020-06-12 15:43:50 +0200

    12/06/2020 Remplacement BASE par Erwann /Timothe/

--- includes/modele/connexion.php
+++ includes/modele/connexion.php
@@ -1,10 +1,16 @@
 <?php
 
-$PARAM_hote='localhost'; // le chemin vers le serveur
+$PARAM_hote='stadjutoogg3.mysql.db'; // le chemin vers le serveur
 $PARAM_port='';
-$PARAM_nom_bd='dbrepas'; // le nom de votre base de donn�es
-$PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
-$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
+$PARAM_nom_bd='stadjutoogg3'; // le nom de votre base de donn�es
+$PARAM_utilisateur='stadjutoogg3'; // nom d'utilisateur pour se connecter
+$PARAM_mot_passe='feuDartifis03'; // mot de passe de l'utilisateur pour se connecter
+
+//$PARAM_hote='localhost'; // le chemin vers le serveur
+//$PARAM_port='';
+//$PARAM_nom_bd='dbrepas'; // le nom de votre base de donn�es
+//$PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
+//$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
 
 try
 {
